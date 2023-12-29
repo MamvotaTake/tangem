5f8da0fbbb0d8e0c5dbfcad3ea6fafe13dc9fda9
Takesure Mamvota <tooyoungmamvota@gmail.com>
2023-12-29 13:49:56 +0700

    Adding icon on button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import {Button} from "./components/Buttons";
+import {AiOutlineClose} from "react-icons/ai";
 
 function App() {
 
   return (
     <>
       <div className="border-4">
-          <Button size="small" type="primary">Shop Now</Button>
-          <Button size="medium" type="secondary">Shop now through Monday</Button>
+          {/*<Button size="small" type="primary">Shop Now</Button>*/}
+          <Button  type="icon" icon={<AiOutlineClose />}/>
       </div>
     </>
   )
